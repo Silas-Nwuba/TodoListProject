df173559d56597331175e0355e2bd0a0d21b3d81
ebukanwuba45@gmail.com <ebukanwuba45@gmail.com>
2022-07-08 16:52:39 +0100

    initial Comit

---
+++ main.js
@@ -0,0 +1,62 @@
+
+    const inputfield = document.getElementById("input-Field");
+    const btn = document.getElementsByClassName("btn")[0];
+    btn.addEventListener("click", () => {
+        if (inputfield.value.trim() != 0) {
+            let localitems = JSON.parse(localStorage.getItem("localitem"));
+            if (localitems === null) {
+                taskList = [];
+            }
+            else {
+                taskList = localitems;
+            }
+            taskList.push(inputfield.value);
+            localStorage.setItem("localitem", JSON.stringify(taskList));
+            inputfield.value = '';
+            showList();
+        }
+    });
+    function showList() {
+        let output = '';
+        let taskdolists = document.querySelector("#table-body")
+        let localitems = JSON.parse(localStorage.getItem("localitem"));
+        if (localitems === null) {
+            taskList = [];
+        }
+        else {
+            taskList = localitems;
+        }
+        taskList.forEach((data, index) => {
+            output +=
+                ` <tr>
+                       <td>${data}</td>
+                         <td>
+                        <button onclick="deletes(${index})"><i class="fa fa-trash"style="color:red"></i> Delete</button>
+                      </td>
+                      </tr>
+                      `
+        });
+        taskdolists.innerHTML = output;
+    }
+    showList();
+    function deletes(index){
+    taskList.splice(index,1);
+    localStorage.setItem("localitem",JSON.stringify(taskList));
+    showList();
+    }
+    let clearbtn = document.getElementById("clearbtn");
+    clearbtn.addEventListener("click", function () {
+        localStorage.clear();
+        showList();
+    });
+    // function deletes(data) {
+
+    //     console.log(data);
+    //     // let localitem = JSON.parse(localStorage.getItem("localitem"));
+    //     // taskList.splice(index, 1);
+    //     // console.log(taskList);
+    //     // localStorage.setItem("localitem", JSON.stringify(taskList));
+    //     //  showList();
+    // }
+ 
+
